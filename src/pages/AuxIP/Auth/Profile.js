5fb96e5baee7de80f0fd6db43354f5627150fd48
adnanhassan1234 <ah5404219@gmail.com>
2022-10-03 18:54:27 +0500

    user table &  btn_css

--- src/pages/AuxIP/Auth/Profile.js
+++ src/pages/AuxIP/Auth/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import HeaderTopBar from '../../../common/header/HeaderTopBar';
 import HeaderTwo from '../../../common/header/HeaderTwo';
@@ -9,14 +9,31 @@ import UserForm from '../../../elements/AuxIP/User/UserForm';
 import SideBar from '../../../elements/AuxIP/Bar/SideBar';
 import { useHistory } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
+
 const User = () => {
+
+
+    const [user, setUser] = useState();
+    const [isLoader, setIsLoader] = useState(false);
+
+
 
     let history = useHistory();
     useEffect(() => {
         if (!localStorage.getItem("auth")) {
             history.push("/login");
         }
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user) {
+            setIsLoader(true);
+            setUser(user.user);
+        }
+
     }, [])
+
+    if (!isLoader)
+        return <div className='loader'><span></span></div>;
+
 
     return (
         <>
@@ -31,28 +48,28 @@ const User = () => {
                             <div className="row mt-0">
 
 
-                                   <SideBar />
+                                <SideBar />
                                 <div className="col-md-10">
                                     <Separator />
                                     <div className="row mb-5">
                                         <div className='col-md-12'>
                                             <div className='card-body'>
                                                 <table>
-                                                <tr>
-                                                       <td>Name</td> 
-                                                       <td>Waleed</td> 
+                                                    <tr>
+                                                        <td>Name</td>
+                                                        <td>{user.name}</td>
                                                     </tr>
                                                     <tr>
-                                                       <td>Email</td> 
-                                                       <td>Waleed</td> 
+                                                        <td>Email</td>
+                                                        <td>{user.email}</td>
                                                     </tr>
                                                     <tr>
-                                                       <td>Role</td> 
-                                                       <td>Waleed</td> 
+                                                        <td>Role</td>
+                                                        <td>{user.roles[0].name}</td>
                                                     </tr>
                                                     <tr>
-                                                       <td>Created At</td> 
-                                                       <td>Waleed</td> 
+                                                        <td>Created At</td>
+                                                        <td>{user.created_at}</td>
                                                     </tr>
                                                 </table>
                                             </div>

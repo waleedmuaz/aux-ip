61e5e29e923bd65002eb28b841608182a5647273
adnanhassan1234 <ah5404219@gmail.com>
2022-10-05 17:56:53 +0500

    Contact Us button

--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -9,10 +9,6 @@ const Nav = () => {
                 <li><Link to="/ip-software">IP Services</Link></li>
                 <li><Link to="/about-us">About Us</Link></li>
                 <li className="ml-auto"><Link to="/contact-us"> <button type='button' className="btn btn-primary">Contact</button> </Link></li>
-                {/* <div className="navbar-nav float-right">
-              <li><Link to="/contact"> <button type='button' className="btn btn-primary">Contact</button> </Link></li>
-            </div> */}
-              
             </ul>
         </>
 
